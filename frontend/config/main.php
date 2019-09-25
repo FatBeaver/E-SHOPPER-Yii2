5b7dec6831c1d7efd4f525051268adb065463015
alexandr <childrenofbodom737@gmail.com>
2019-09-25 20:46:58 +0300

    Pgination

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -41,6 +41,7 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'category/<id:\d+>/page/<page:\d+>' => 'category/view',
                 'category/<id:\d+>' => 'category/view',
             ],
         ],
--- frontend/controllers/CategoryController.php
+++ frontend/controllers/CategoryController.php
@@ -6,6 +6,7 @@ use Yii;
 use common\controllers\AppController;
 use frontend\models\Category;
 use frontend\models\Product;
+use yii\data\Pagination;
 
 class CategoryController extends AppController
 {
@@ -21,12 +22,22 @@ class CategoryController extends AppController
     public function actionView()
     {   
         $id = Yii::$app->request->get('id');
-        $products = Product::find()->where(['category_id' => $id])->all();
+        //$products = Product::find()->where(['category_id' => $id])->all();
+        $query = Product::find()->where(['category_id' => $id]);
+        $pages = new Pagination([
+            'totalCount' => $query->count(),
+            'pageSize' => 3,
+            'forcePageParam'  => false,
+            'pageSizeParam' => false
+        ]);
+
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
         $category = Category::findOne($id);
         $this->setMeta("E-SHOPPER | " . $category->name, $category->keywords, $category->description);
 
         return $this->render('view', [
             'products' => $products,
+            'pages' => $pages,
             'category' => $category,
         ]);
     }
