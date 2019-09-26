1efe3cc2aac46a5244abd9019166dd00591f6466
alexandr <childrenofbodom737@gmail.com>
2019-09-26 22:40:38 +0300

    ready cart Product

---
+++ frontend/controllers/ProductController.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace frontend\controllers;
+
+use Yii;
+use common\controllers\AppController;
+use frontend\models\Controller;
+use frontend\models\Product;
+
+class ProductController extends AppController 
+{
+    public function actionView()
+    {
+        $id = Yii::$app->request->get('id');
+
+        $product = Product::find()->with('category')->where(['id' => $id])->one();
+        $categoryProducts = Product::find()->where(['category_id' => $product->category->id])->all();
+        $hitProducts = Product::find()->where(['hit' => 1])->limit(9)->orderBy('id DESC')->all();
+        
+        $this->setMeta("E-SHOPPER | " . $product->name, $product->keywords, $product->description);
+
+        return $this->render('view', [
+            'product' => $product,
+            'categoryProducts' => $categoryProducts,
+            'hitProducts' => $hitProducts
+        ]);
+    }
+}
